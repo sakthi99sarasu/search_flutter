9e4cc9f02200a3c78e9ec3cea7f1b812cffc4662
unknown <sakthi99sarasu@gmail.com>
2022-06-17 01:09:14 +0530

    first commit

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class Config {
   static const String appName = "search";
   static const String apiURL = "192.168.43.57:4002";
-  static const String keywordURL = "/api/keywords/all_keywords1";
-  static const String resultURL = "/api/keywords/all_keywords";
+  static const String keywordURL = "/search/retrieveresult";
+  static const String resultURL = "/search/retrievedetails";
   static const String login = "/api/keywords/login";
 }
